5b271e30fa2a17453ef0c108c16d59a97c1d5665
egzidev <egzon.gollopeni@gmail.com>
2023-03-10 03:38:36 +0100

    Changed design of the Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Loading = () => {
-   return  <div className="deel-autocomplete__loading">Loading...</div>
+    return (
+        <ul className="deel-autocomplete__list deel-autocomplete__list-open">
+            <li>
+                <div className="deel-autocomplete__loading">Loading...</div>
+            </li>
+        </ul>
+    )
 }
 
 export default Loading;
